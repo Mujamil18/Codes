907aeb06faff64f12622d17ea79a88a710080dac
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-02-05 12:25:32 +0530

    Create 24.Expression24.java

---
+++ 24.Expression24.java
@@ -0,0 +1,9 @@
+public class Expression24 {
+    public static void main(String[] args){
+        int y=10, z;
+        z= ( ++y * (y++ + 5));
+        System.out.println(z);
+        System.out.println(y);
+
+    }
+}
