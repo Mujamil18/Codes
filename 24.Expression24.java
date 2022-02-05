0be064aaa18de620a06f5cedfe85c61faca467fb
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-02-05 12:27:01 +0530

    Update 24.Expression24.java

--- 24.Expression24.java
+++ 24.Expression24.java
@@ -2,8 +2,8 @@ public class Expression24 {
     public static void main(String[] args){
         int y=10, z;
         z= ( ++y * (y++ + 5));
-        System.out.println(z);
-        System.out.println(y);
+        System.out.println(z); // 176
+        System.out.println(y); // 12
 
     }
 }
