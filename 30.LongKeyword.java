b145ea99a0fa325684c24396108d8e360bf997ef
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-02-05 17:21:14 +0530

    Create 30.LongKeyword.java

---
+++ 30.LongKeyword.java
@@ -0,0 +1,9 @@
+
+public class LongKeyword {
+    public static void main(String[] args){
+        long num= 1234567899876543210L;
+        System.out.println(num);
+        System.out.println("Size of long datatype is " + (Long.SIZE/8) + " bytes");
+    }
+
+}
