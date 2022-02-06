5b56a905cc96c82c6cb5e158a7459c895a1f3be5
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-02-06 20:35:15 +0530

    Create 44.ASCII.java

---
+++ 44.ASCII.java
@@ -0,0 +1,11 @@
+
+public class PrintAtoZ {
+    public static void main(String[] args){
+
+        for (int i=65; i<=90; i++)
+        {
+            char ch=(char)i;
+           System.out.println(ch + " ");
+        }
+    }
+}
