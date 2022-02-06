4b061500e7eeb6070b4bb0bb175bfcc5582f15e1
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-02-06 17:29:54 +0530

    Create 33.VowelConst.java

---
+++ 33.VowelConst.java
@@ -0,0 +1,18 @@
+{
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         System.out.println(" Enter an Alphabet");
+         char ch = sc.next().charAt(0);
+         switch (ch) {
+             case 'a':
+             case 'e':
+             case 'i':
+             case 'o':
+             case 'u':
+                 System.out.println(ch + " is a Vowel");
+                 break;
+             default:
+                 System.out.println(ch + " is a Consonant");
+         }
+     }
+ }
