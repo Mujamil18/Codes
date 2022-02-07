8eb2cc38f2185f249a868f91a8e8b01e9966c728
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-02-07 08:05:26 +0530

    Create 37.AlphabetorNot.java

---
+++ 37.AlphabetorNot.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class AlphabetorNot {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter an Alphabet");
+        char ch= sc. next().charAt(0);
+        if(ch>= 65 && ch<=90 || ch>=97 && ch<=122)
+            System.out.println(ch+ " is an Alphabet");
+        else
+            System.out.println(ch + " is not an Alphabet");
+
+    }
+}
