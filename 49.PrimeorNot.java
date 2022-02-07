28c9214a1da7e36f77726b550c6b42bbd37dbe66
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-02-07 11:21:41 +0530

    Update 49.PrimeorNot.java

--- 49.PrimeorNot.java
+++ 49.PrimeorNot.java
@@ -3,12 +3,25 @@ public class PrimeorNot {
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
         int num= sc.nextInt();
-        for(int i=2; i<=num/2; ++i) {
-            if (num % i == 0) {
-                System.out.println(num + " is not a prime number");
-            }
+        int count=0;
+        for(int i=1; i<=num; i++)
+        {
+            if(num%i==0)
+                count++;
         }
-        System.out.println(num + " is a Prime Number");
+        if(count==2)
+           System.out.println(num + "is a Prime number");
+        else
+            System.out.println(num + " is not a Prime number");
+
+
+        //for(int i=2; i<=num/2; ++i) {
+        //  if (num % i == 0) {
+        //    System.out.println(num + " is not a prime number");
+        //}
+        //}
+        //System.out.println(num + " is a Prime Number");
         }
     }
+
 
