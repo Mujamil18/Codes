9dde7f63d574717c7d7338d7be0d3aabcbd73820
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-02-08 21:35:26 +0530

    Update 56.SumofPrime.java

--- 56.SumofPrime.java
+++ 56.SumofPrime.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class SumofPrime {
-    public static boolean Prime(int n) {
+    public static boolean isPrime(int n) {
         int p;
         for (p = 2; p <= n / 2; p++) {
             if (n % p == 0)
@@ -14,7 +14,7 @@ public class SumofPrime {
         System.out.println("Enter a number");
         int num = sc.nextInt();
         for (int i = 2; i < num / 2; i++) {
-            if (Prime(i) && Prime(num - i)) ;
+            if (isPrime(i) && isPrime(num - i)) ;
             {
                 System.out.println(num + " is a sum of two prime numbers");
                 return;
