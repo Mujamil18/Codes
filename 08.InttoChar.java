990fe655aa57ba718dba668fd41faabfa558508b
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-02-10 16:16:30 +0530

    Update 08.InttoChar.java

--- 08.InttoChar.java
+++ 08.InttoChar.java
@@ -2,6 +2,6 @@ public class ChartoInt {
     public static void main(String args[]){
         int number55=55;
         char character55=(char) number55;
-        System.out.println(number55);
+        System.out.println(character55);
     }
 }
