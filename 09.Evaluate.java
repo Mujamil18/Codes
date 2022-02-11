d894a36de89a1999dfbf490b909c83e2a5575cf0
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-02-11 09:09:43 +0530

    Update 09.Evaluate.java

--- 09.Evaluate.java
+++ 09.Evaluate.java
@@ -1,8 +1,8 @@
 public class Evaluate9 {
     public static void main(String[] args){
-        System.out.println(7+7);
-        System.out.println(7+7.0);
-        System.out.println(7+'7');
+        System.out.println(7+7); //14
+        System.out.println(7+7.0); //14.0
+        System.out.println(7+'7'); // 7+55=62
 
     }
 }
