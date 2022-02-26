2c0451965de3386069d00c8ca586fefaf83f11f3
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-02-26 15:43:36 +0530

    Update 33.VowelConst.java

--- 33.VowelConst.java
+++ 33.VowelConst.java
@@ -4,15 +4,23 @@
          System.out.println(" Enter an Alphabet");
          char ch = sc.next().charAt(0);
          switch (ch) {
-             case 'a':
-             case 'e':
-             case 'i':
-             case 'o':
-             case 'u':
-                 System.out.println(ch + " is a Vowel");
-                 break;
-             default:
-                 System.out.println(ch + " is a Consonant");
+                   
+             case 'a', 'e', 'i', 'o', 'u', 'A','E', 'I', 'O' , 'U' -> System.out.println(ch + " is a Vowel");
+             default -> System.out.println(ch + " is a Consonant");
          }
      }
  }
+
+//              case 'a':
+//              case 'e':
+//              case 'i':
+//              case 'o':
+//              case 'u':
+//                  System.out.println(ch + " is a Vowel");
+//                  break;
+//              default:
+//                  System.out.println(ch + " is a Consonant");
+//          }
+//      }
+//  }
+
