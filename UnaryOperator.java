59f94383c230fdcbbfde271ae16edf9c8c9620d0
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-03-06 22:07:54 +0530

    Update and rename IncDecOperators.java to UnaryOperator.java

--- UnaryOperator.java
+++ UnaryOperator.java
@@ -1,6 +1,14 @@
-public class IncDecOperators {
+public class UnaryOperators {
     public static void main(String[] args)
     {
+        //Unary plus
+        int num= +2;
+        System.out.println(num);
+
+        //Unary Minus
+        int sum = 4 + -2;
+        System.out.println(sum);
+
         int m= 10, n=20;
         int x= m++ + ++n; //m is 10 and n is 21 (10+21=31)
         System.out.println(x);
@@ -11,5 +19,8 @@ public class IncDecOperators {
         System.out.println(y);
         System.out.println(a + " " + b); // a is decremented here
 
+        //Logical Complement
+        int p=10, q=12;
+        System.out.println((a!=b));
     }
 }
