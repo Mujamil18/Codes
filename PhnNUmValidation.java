8e7e98e347aa0808efffcec6e94d448fadf80ee4
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-04-06 23:01:50 +0530

    Create PhnNUmValidation.java

---
+++ PhnNUmValidation.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PhnNumValidation
+{
+    public static void main(String[] args)
+    {
+        Scanner in= new Scanner(System.in);
+        System.out.println("Enter a Phone Number");
+      String phoneNumber= in.nextLine();
+      Matcher num= Pattern.compile("[+-0123456789]").matcher(phoneNumber);
+      if(num.find())
+      {
+          System.out.println("Valid Phone Number");
+      }
+      else
+      {
+          System.out.println("Invalid Phone Number");
+      }
+    }
+}
