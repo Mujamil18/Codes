b612fb56869ec9f4cab288a817e8dbeb32e401e1
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-04-07 20:46:51 +0530

    Create MaxXOR.java

---
+++ MaxXOR.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class XOR {
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+//        int n= sc.nextInt();
+        int[] arr= new int[]{3, 10, 5, 25, 2, 8};
+        ArrayList<Integer> XOR= new ArrayList<>();
+        for(int i=0; i<arr.length-1; i++)
+        {
+//            arr[i] = sc.nextInt();
+            for(int j=i+1; j<arr.length; j++)
+            {
+            System.out.println(j);
+                XOR.add(arr[i] ^ arr[j]);
+            }
+        }
+        System.out.println("The maximum result of two Numbers is: " + Collections.max(XOR));
+    }
+}
