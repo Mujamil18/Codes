f0acc3e7a02bfba3483fcd27c29b83d6229b2dea
Mujamil <61487444+Mujamil18@users.noreply.github.com>
2022-04-07 12:34:21 +0530

    Update EmailValidation.java

--- EmailValidation.java
+++ EmailValidation.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class EmailValidation
 {
@@ -9,9 +7,9 @@ public class EmailValidation
         Scanner in= new Scanner(System.in);
         System.out.println("Enter a Mail Id");
         String mailId= in.nextLine();
-        mailId= mailId.toLowerCase();
-        Matcher isCorrect= Pattern.compile("[a-z_@.]").matcher(mailId);
-        if(isCorrect.find())
+        String regex= "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+        boolean isMatched= mailId.matches(regex);
+        if(isMatched)
         {
             System.out.println("Valid Mail Id");
         }
